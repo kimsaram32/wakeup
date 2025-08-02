4809eeb2593de55d373b7dfdf0fa459ec532ddef
kimsaram32 <kimsaram32@proton.me>
2025-08-02 23:21:47 +0900

    fix: fix streak command on weekends

--- src/handlers/interaction-handlers/streak.ts
+++ src/handlers/interaction-handlers/streak.ts
@@ -4,6 +4,7 @@ import { guildSettings } from "../../entities/guild-settings.ts";
 import { runRecord } from "../../entities/run-record.ts";
 import { InteractionHandler } from "../types.ts";
 import { Config } from "../../config.ts";
+import { isWeekday } from "../../utils/is-weekday.ts";
 
 function createRecordMap(
   records: { userId: string; date: Temporal.PlainDate }[],
@@ -62,7 +63,13 @@ export const showStreakInteractionHandler: InteractionHandler = {
       let streak = 0;
       let currentDate = Temporal.PlainDate.from(today);
 
-      while (set.has(currentDate.toString())) {
+      while (true) {
+        while (!isWeekday(currentDate.dayOfWeek)) {
+          currentDate = currentDate.subtract(aDay);
+        }
+        if (!set.has(currentDate.toString())) {
+          break;
+        }
         currentDate = currentDate.subtract(aDay);
         streak++;
       }
@@ -70,10 +77,9 @@ export const showStreakInteractionHandler: InteractionHandler = {
       return streak;
     });
 
-    const sortedMemberIdAndStreaks = zip(targetMemberIds, streaks).sort((
-      a,
-      b,
-    ) => b[1] - a[1]);
+    const sortedMemberIdAndStreaks = zip(targetMemberIds, streaks).sort(
+      (a, b) => b[1] - a[1],
+    );
 
     interaction.reply({
       embeds: [
@@ -81,8 +87,8 @@ export const showStreakInteractionHandler: InteractionHandler = {
           .setTitle("점호 스트릭 현황")
           .setDescription(
             sortedMemberIdAndStreaks
-              .map(([id, streak], index) =>
-                `${index + 1}. <@${id}> (${streak}일)`
+              .map(
+                ([id, streak], index) => `${index + 1}. <@${id}> (${streak}일)`,
               )
               .join("\n"),
           ),
